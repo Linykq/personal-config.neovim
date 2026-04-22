b89cd013b45cd4e4716e3b84c3f22cb961df0299
Linykq <yukunlin590@gmail.com>
2026-04-22 16:08:21 -0500

    feat(telescope): plugin for searching file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -91,6 +91,15 @@ require("lazy").setup({
     -- Git sign
     {
       "lewis6991/gitsigns.nvim",
+    },
+    -- File searcher
+    {
+      'nvim-telescope/telescope.nvim', version = '0.2.1',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+      }
     }
   },
   -- Configure any other settings here. See the documentation for more details.
