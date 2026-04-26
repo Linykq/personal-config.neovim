1dbac1c2e48b223bc69f354a609a00b31ae14e73
Linykq <yukunlin590@gmail.com>
2026-04-26 03:11:36 -0500

    update(lsp): add supported languages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,8 +10,16 @@ require("mason").setup({
 
 require("mason-lspconfig").setup({
   ensure_installed = {
-    "lua_ls",
-  },
+    "lua_ls",           -- Lua
+    "clangd",           -- C, C++
+    "jdtls",            -- Java
+    "gopls",            -- Go
+    "pyright",          -- Python (or "pylsp", "basedpyright")
+    "ts_ls",            -- TypeScript, JavaScript (formerly "tsserver")
+    "cssls",            -- CSS
+    "html",             -- HTML
+    "texlab",           -- LaTeX
+  }
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
